c944101494f495fe4101b357a7a4cfc10a0b8ecb
prakashjamer <32328991+prakashjamer@users.noreply.github.com>
2023-12-05 12:14:03 +0530

    Update create-ec2.sh

--- shell-scripting/roboshop/create-ec2.sh
+++ shell-scripting/roboshop/create-ec2.sh
@@ -13,7 +13,7 @@ fi
 
 COMPONENT=$1
 ENV=$2
-HOSTEDZONEID="Z036562835VZ0HPAX8JK7"   #  old  hosted zone  id Z031297333JO38PNHPROR
+HOSTEDZONEID="Z03497188CKRADFI9ZYM"   #  old  hosted zone  id Z031297333JO38PNHPROR
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-allow-all" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
 INSTANCE_TYPE="t2.micro"
